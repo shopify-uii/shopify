94685dc3f4803ebb69a4ace5c7f1db7453030991
shopify-uii <chovatiyaasmita73@gmail.com>
2026-03-27 10:33:32 +0530

    Update shopify.js

--- shopify.js
+++ shopify.js
@@ -1,1 +1,15 @@
-console.log('shopify');
+// shopify.js
+
+const ShopifyUI = {
+  init() {
+    console.log("Shopify UI initialized");
+  },
+
+  greet(name = "User") {
+    console.log(`Hello, ${name}!`);
+  },
+
+  version: "1.0.0"
+};
+
+export default ShopifyUI;
